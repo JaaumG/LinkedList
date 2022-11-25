21e393269f5f00f3fd9b588f1633a89587d2fa5f
João Guilherme <jaaumgui@gmail.com>
2022-11-25 11:07:50 -0300

    Função começo da lista, corigido função get.
    Adicionado Função de adicionar um valor no começo da lista, corrigido a função de get, agora esta retornando o valor correto

--- main.c
+++ main.c
@@ -17,38 +17,70 @@ LinkedList * InserirLista(LinkedList * list, int val){
     }
 
 }
+LinkedList * getList(LinkedList * list){
+    LinkedList * novaList = list;
+    int size = 0;
+    while(novaList->next!=NULL){
+        size+=1;
+        novaList = novaList->next;
+    }
+    return novaList;
+}
+int getSize(LinkedList * list){
+    int size = 0;
+    while(list->next!=NULL){
+        list = (LinkedList *) list->next;
+        size+=1;
+    }
+    return size;
+}
+
+/*
+ * Retorna o valor da lista na possição passada nos parametros
+ */
+int get(LinkedList * list, int pos ){
+    int i, item =getSize(list)-pos;
+    if(item<0){
+        printf("\nA lista e menor que %d, retornando ultimo valor da lista.\n", pos);
+    }
+    for(i = 0; i<item; i++){
+        list = (LinkedList *) list->next;
+    }
+    return list->value;
+}
+
+
+LinkedList * InserirComecoLista(LinkedList * list, int val){
+    LinkedList * novaList = InicializarLista();
+    novaList = InserirLista(novaList, val);
+    LinkedList * oldList = list;
+    list = getList(list);
+    list->next = (struct LinkedList *) novaList;
+    return oldList;
+}
 LinkedList * ProximoItem(LinkedList list){
     return (LinkedList *) list.next;
 }
 
-int get(LinkedList * list, int pos ){
-    LinkedList * novaList = list;
-    int size = 0;
-    while(novaList!=NULL){
-        size+=1;
-        novaList = novaList->next;
-    }
-    int i;
-    novaList = list;
-    for(i = 0; i++; i<size-pos){
-        novaList = novaList->next;
-    }
-    return novaList->value;
-}
+
 void ImprimeList(LinkedList * L ){
     while(L!=NULL){
         printf("[%d]->", L->value);
         L = L->next;
     }
 }
+
 int main() {
     LinkedList * L;
     L = InicializarLista();
-    L = InserirLista(L, 10);
+    L = InserirLista(L, 1);
     L = InserirLista(L, 10);
     L = InserirLista(L, 100);
     L = InserirLista(L, 1000);
     L = InserirLista(L, 10000);
     ImprimeList(L);
-    printf("\n%d", get(L, 2));
+    printf("\nvalor: %d\n", get(L, 5));
+    L = InserirComecoLista(L, 5);
+    ImprimeList(L);
+    printf("\n%d\n", get(L, 2));
 }
